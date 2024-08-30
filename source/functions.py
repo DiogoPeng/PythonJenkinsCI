6a0f312145d69de6e2c44490637bc96913cfaf27
DiogoPeng <dbpgo@iscte-iul.pt>
2024-08-30 01:40:19 +0100

    1st commit

---
+++ source/functions.py
@@ -0,0 +1,15 @@
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1,n2):
+    return n1-n2
+
+def mult(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    if(n2!=0):
+        return n1/n2
+    else:
+        print("n2 n pode ser negativo")
+        return
---
+++ tests/tests.py
@@ -0,0 +1,16 @@
+import source.functions as funcs
+
+def test_add():
+    assert funcs.add(1,2) == 3
+
+def test_sub():
+    assert funcs.sub(2,1) == 1
+
+def test_divide():
+    assert funcs.divide(10,2) == 5
+
+def test_mult():
+    assert funcs.mult(2,10) == 20
+
+
+
